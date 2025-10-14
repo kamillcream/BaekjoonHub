bc6640968496223f89cdbeb592fec83edccca1a9
Jinyoung Park <glitt5384@naver.com>
2025-10-15 02:49:04 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 67.39 ms, Memory: 38 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+def solution(participant, completion):
+    p_counter = Counter(participant)
+    c_counter = Counter(completion)
+    
+    diff = p_counter - c_counter
+    
+    return list(diff.keys())[0]
