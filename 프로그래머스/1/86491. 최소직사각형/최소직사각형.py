891662c450d5b8a70cfd45b1828f505525aab421
Jinyoung Park <glitt5384@naver.com>
2025-11-01 23:40:00 +0900

    [level 1] Title: 최소직사각형, Time: 4.42 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,13 @@
+def solution(sizes):
+    max_x = 0
+    max_y = 0
+    
+    for x, y in sizes:
+        if x < y:
+            x,y = y,x
+        
+        max_x = max(max_x, x)
+        max_y = max(max_y, y)
+        
+        
+    return max_x * max_y
